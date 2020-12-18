741a462be4ed83b2c96273a0c7a9380431b5e729
jessedd <jesusedd_41@outlook.com>
2020-12-18 14:02:34 -0600

    Agregué en Untils la clase Auth, modelo SystemsParametersVM y TypeServiceVM y comencé con el get para el catálogo TypeServices

---
+++ GFD.Siscom.Enrollment/Models/TypeServiceVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GFD.Siscom.Enrollment.Models
+{
+    public class TypeServiceVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool isActive { get; set; }
+    }
+}
