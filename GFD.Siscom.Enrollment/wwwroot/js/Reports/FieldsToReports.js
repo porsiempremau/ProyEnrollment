fef20722494afe8d1b6f65bbca85a6a065d0e9be
jessedd <jesusedd_41@outlook.com>
2021-02-24 10:14:25 -0600

    Se acomodá el campo fields para utilizarlo con los demás reportes

---
+++ GFD.Siscom.Enrollment/wwwroot/js/Reports/FieldsToReports.js
@@ -0,0 +1,72 @@
+﻿function GetFieldsToReport(nameReport) {
+    var fields = [];
+    switch (nameReport) {
+        case "Orders":
+            fields = [
+                {
+                    caption: "División",
+                    dataField: "division",
+                    area: "row",
+                },
+                {
+                    caption: "Folio",
+                    dataField: "folio",
+                    dataType: "text",
+                    area: "row",
+                },
+                {
+                    caption: "Descripción",
+                    dataField: "description",
+                    area: "row"
+                },
+                {
+                    caption: "Se creó",
+                    dataField: "fechaOS",
+                    format: "text",
+                    area: "row"
+                },
+                {
+                    caption: "Cliente",
+                    dataField: "cliente",
+                    dataType: "text",
+                    area: "row"
+                }, {
+                    caption: "Estado",
+                    dataField: "estado",
+                    dataType: "text",
+                    area: "column"
+                }, {
+                    caption: "Descuento",
+                    dataField: "descuento",
+                    dataType: "number",
+                    summaryType: "sum",
+                    format: "$ #,##0.##",
+                    area: "data"
+                }, {
+                    caption: "Subtotal",
+                    dataField: "subtotal",
+                    dataType: "number",
+                    summaryType: "sum",
+                    format: "$ #,##0.##",
+                    area: "data"
+                }, {
+                    caption: "I.V.A.",
+                    dataField: "iva",
+                    dataType: "number",
+                    summaryType: "sum",
+                    format: "$ #,##0.##",
+                    area: "data"
+                }, {
+                    caption: "Total",
+                    dataField: "total",
+                    dataType: "number",
+                    summaryType: "sum",
+                    format: "$ #,##0.##",
+                    area: "data"
+                }
+            ];
+            break;
+    }
+
+    return fields;
+}
